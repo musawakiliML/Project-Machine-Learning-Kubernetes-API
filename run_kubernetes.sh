95e10f5d526b2f03fa17f68a4052edde61f62de7
Ubuntu <ubuntu@ip-172-31-3-154.ec2.internal>
2023-03-12 20:37:03 +0000

    update

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,14 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath="musawakiliml/udacityproject"
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacityproject --image=$dockerpath --port=80 --labels app=udacityproject
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacityproject 8000:80
